6d1db4524c19090113d4c94a99c22c2eb6d27c2a
Hatem <hattoum96@gmail.com>
2019-08-19 04:22:09 +0200

    fixd the bs

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const cors = require("cors")
+
+app.use(cors())
+
+
 
 app.use(bodyParser.json());
-
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -28,3 +32,4 @@ mongoose
   .catch(err => {
     console.log(err);
   });
+
