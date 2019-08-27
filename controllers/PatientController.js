cccab85b6ba3a4a3e7cefdf407a53ca58afd7b01
Hatem <hattoum96@gmail.com>
2019-08-27 21:37:48 +0200

    fixing delete

--- controllers/PatientController.js
+++ controllers/PatientController.js
@@ -2,7 +2,6 @@ const Patient = require("../models/Patient");
 const SortResults = require("../utils/SortResults")
 
 exports.addPatient = (req, res, next) => {
-  console.log(req.body);
   const name = req.body.patient.name;
   const age = req.body.patient.age;
   const gender = req.body.patient.gender;
@@ -17,7 +16,6 @@ exports.addPatient = (req, res, next) => {
     visits: visits
   })
     .then(result => {
-      console.log(result);
       res.status(200).json({ result });
     })
     .catch(err => console.log(err));
@@ -31,9 +29,6 @@ exports.updatePatient = (req, res, next) => {
   const visits = req.body.patient.visits;
   const _id = req.body.patient._id;
 
-  // Patient.findOne({ _id: id })
-  //   .then(p => {
-  //     console.log(p);
       Patient.update({_id: _id},{
         name: name,
         age: age,
@@ -46,14 +41,13 @@ exports.updatePatient = (req, res, next) => {
           res.status(200).json({ msg: "updated" });
         })
         .catch(err => console.log(err));
-    // })
-//     .catch(err => console.log(err));
+
 };
 
 exports.deletePatient = (req, res, next) => {
-  const id = req.body.patient.id;
+  const id = req.body._id;
 
-  Patient.deleteOne({ id: id })
+  Patient.deleteOne({ _id: id })
     .then(result => {
       console.log(result);
       res.status(200).json({ msg: "Deleted" });
@@ -66,10 +60,8 @@ exports.deletePatient = (req, res, next) => {
 
 exports.getPatient = (req, res, next) => {
   const id = req.body.id;
-  console.log(id);
   Patient.findOne({ _id: id })
     .then(p => {
-      console.log(p);
       res.status(200).json({ patient: p });
     })
     .catch(err => {
